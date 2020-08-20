2c2706f8f649d5afbe58dac5e688862c0339072d
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2020-08-20 22:34:52 +0200

    Create the first test for Channels Writes-Reads

--- modules/common-infrastructure/src/it/scala/io/branchtalk/IOTest.scala
+++ modules/common-infrastructure/src/it/scala/io/branchtalk/IOTest.scala
@@ -3,9 +3,30 @@ package io.branchtalk
 import cats.effect.{ ContextShift, IO, Timer }
 
 import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
 
 trait IOTest {
 
   protected implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
   protected implicit val timer:        Timer[IO]        = IO.timer(ExecutionContext.global)
+
+  implicit class IOTestOps[T](private val io: IO[T]) {
+
+    def eventually(retries: Int = 40, duration: FiniteDuration = 5.seconds): IO[T] = {
+      val timeout = IO.sleep(duration) >> IO.raiseError(
+        new Exception(s"IO failed to succeed: exceeded duration $duration")
+      )
+
+      def retry(attemptsLeft: Int): PartialFunction[Throwable, IO[T]] = {
+        case cause: Throwable =>
+          if (attemptsLeft <= 0) IO.raiseError(new Exception(s"IO failed to succeed: exceeded retries $retries", cause))
+          else io.handleErrorWith(retry(attemptsLeft - 1))
+      }
+
+      IO.race(io.handleErrorWith(retry(retries)), timeout).map {
+        case Left(value) => value
+        case Right(_)    => ??? // impossible
+      }
+    }
+  }
 }
---
+++ modules/discussions-impl/src/it/scala/io/branchtalk/discussions/ChannelReadsWritesSpec.scala
@@ -0,0 +1,52 @@
+package io.branchtalk.discussions
+
+import cats.effect.{ IO, Resource }
+import io.branchtalk.{ IOTest, ResourcefulTest }
+import io.branchtalk.discussions.model.Channel
+import io.branchtalk.shared.infrastructure._
+import io.branchtalk.shared.infrastructure.DoobieSupport._
+import io.branchtalk.shared.models.UUIDGenerator
+import org.specs2.mutable.Specification
+
+final class ChannelReadsWritesSpec extends Specification with IOTest with ResourcefulTest {
+
+  private implicit val uuidGenerator: UUIDGenerator = UUIDGenerator.FastUUIDGenerator
+
+  // populated by resources
+  private var transactor:        Transactor[IO]        = null
+  private var discussionsReads:  DiscussionsReads[IO]  = null
+  private var discussionsWrites: DiscussionsWrites[IO] = null
+
+  override protected def testResource: Resource[IO, Unit] =
+    for {
+      domainCfg <- TestDiscussionsConfig.loadDomainConfig[IO]
+      xa <- new PostgresDatabase(domainCfg.database).transactor[IO]
+      reads <- DiscussionsModule.reads[IO](domainCfg)
+      writes <- DiscussionsModule.writes[IO](domainCfg)
+    } yield {
+      transactor        = xa
+      discussionsReads  = reads
+      discussionsWrites = writes
+    }
+
+  "Channel Reads & Writes" should {
+
+    "create Channel and read it" in {
+      discussionsWrites.runProjector
+        .use { projector =>
+          // given
+          val creationData = List.empty[Channel.Create]
+
+          // when
+          for {
+            _ <- projector.start
+            scheduled <- creationData.traverse(discussionsWrites.channelWrites.createChannel)
+            channels <- scheduled.map(_.id).traverse(discussionsReads.channelReads.requireById).eventually()
+          } yield (scheduled.map(_.id).toSet === channels.map(_.id).toSet)
+        }
+        .unsafeRunSync()
+    }
+  }
+
+  // TODO: test rest of Channels API
+}
