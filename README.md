22f433e1a6ac67e3343db8eeec2361f69038748d
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2021-01-29 18:07:05 +0100

    Comment scripts

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# branchtalk
+# BranchTalk
 
 Demo of how scalable implementation of Reddit-like service could be made
 in Scala to demonstrate some patterns and principles.
--- scripts/prepare-test-results-for-ci.sh
+++ scripts/prepare-test-results-for-ci.sh
@@ -1,14 +1,16 @@
 #!/bin/zsh
 
+# for Shippable only
+
 if [ -n "\$SHIPPABLE" ]; then
   ls modules/*/target/test-reports/* 1> /dev/null 2>&1
   if [ \$? -eq 0 ]; then
     mkdir -p shippable/testresults
     cp modules/*/target/test-reports/* \$PWD/shippable/testresults/
   fi
-  cat target/scala-2.12/coverage-report/cobertura.xml 1> /dev/null 2>&1
+  cat target/scala-2.13/coverage-report/cobertura.xml 1> /dev/null 2>&1
   if [ \$? -eq 0 ]; then
     mkdir -p shippable/codecoverage
-    cp target/scala-2.12/coverage-report/cobertura.xml \$PWD/shippable/codecoverage/
+    cp target/scala-2.13/coverage-report/cobertura.xml \$PWD/shippable/codecoverage/
  fi
 fi
