2e8c3b3bec82e63eda1e8c5e4411d416e5e1d510
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2024-08-12 09:26:42 +0200

    Make app compile

--- modules/app/src/main/scala/io/branchtalk/logging/IOMDCAdapter.scala
+++ modules/app/src/main/scala/io/branchtalk/logging/IOMDCAdapter.scala
@@ -2,31 +2,32 @@ package io.branchtalk.logging
 
 import cats.effect.{ IO, IOLocal }
 
-import java.{ util => ju }
-import ch.qos.logback.classic.util.LogbackMDCAdapter
+import java.util as ju
+import org.slf4j.spi.MDCAdapter
 
 import scala.jdk.CollectionConverters.*
 
 // Based on solution described by OlegPy in https://olegpy.com/better-logging-monix-1/
 // Using experimental hack: https://gist.github.com/MateuszKubuszok/d506706ee3c9b4c2291d47279f619523
-final class IOMDCAdapter(local: IOLocal[MDC.Ctx]) extends LogbackMDCAdapter {
+final class IOMDCAdapter(local: IOLocal[MDC.Ctx]) extends MDCAdapter {
 
   private def getMDC:                          MDC.Ctx = IOGlobal.getCurrent(local).getOrElse(Map.empty[String, String])
   private def setMDC(mdc: MDC.Ctx):            Unit    = IOGlobal.setTemporarily(local, mdc)
   private def update(f:   MDC.Ctx => MDC.Ctx): Unit    = setMDC(f(getMDC))
 
+  override def put(key: String, `val`: String): Unit = update(_.updated(key, `val`))
   @SuppressWarnings(Array("org.wartremover.warts.Null")) // talking to Java interface
-  override def get(key:    String):                String = getMDC.get(key).orNull.asInstanceOf[String]
-  override def put(key:    String, `val`: String): Unit   = update(_.updated(key, `val`))
-  override def remove(key: String):                Unit   = update(_.removed(key))
+  override def get(key:    String): String = getMDC.get(key).orNull
+  override def remove(key: String): Unit   = update(_.removed(key))
+  override def clear():             Unit   = setMDC(Map.empty)
 
-  override def clear():             Unit                   = setMDC(Map.empty)
   override def getCopyOfContextMap: ju.Map[String, String] = getMDC.asJava
-  override def setContextMap(contextMap: ju.Map[String, String]): Unit = setMDC(contextMap.asScala.toMap)
-  def setContextMap(contextMap: ju.Map[?, ?]): Unit = setMDC(contextMap.asScala.toMap.asInstanceOf[MDC.Ctx])
+  override def setContextMap(contextMap: ju.Map[String, String] @unchecked): Unit = setMDC(contextMap.asScala.toMap)
 
-  override def getPropertyMap: ju.Map[String, String] = getMDC.asJava
-  override def getKeys:        ju.Set[String]         = getMDC.asJava.keySet()
+  override def pushByKey(key:           String, value: String): Unit             = ???
+  override def popByKey(key:            String):                String           = ???
+  override def getCopyOfDequeByKey(key: String):                ju.Deque[String] = ???
+  override def clearDequeByKey(key:     String):                Unit             = ???
 }
 object IOMDCAdapter {
 
