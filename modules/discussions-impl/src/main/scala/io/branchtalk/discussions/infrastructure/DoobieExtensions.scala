08724b3ff6fb0c43f4051fa5faf4474a1c8df919
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2024-08-19 00:59:26 +0200

    Fix most warnings

--- modules/discussions-impl/src/main/scala/io/branchtalk/discussions/infrastructure/DoobieExtensions.scala
+++ modules/discussions-impl/src/main/scala/io/branchtalk/discussions/infrastructure/DoobieExtensions.scala
@@ -1,21 +1,21 @@
 package io.branchtalk.discussions.infrastructure
 
+import cats.Show
 import io.branchtalk.discussions.model.{ Post, Vote }
 import io.branchtalk.shared.infrastructure.DoobieSupport.{ *, given }
 import io.branchtalk.shared.model.branchtalkLocale
 
 object DoobieExtensions {
 
+  private given [A: Show]: Show[Array[A]] = _.map(_.show).mkString(", ")
+
+  @SuppressWarnings(Array("org.wartremover.warts.Throw"))
   given postContentTypeMeta: Meta[Post.Content.Type] = pgEnumString(
     "post_content_type",
     name =>
       Post.Content.Type.values
         .find(_.entryName.equalsIgnoreCase(name))
-        .getOrElse(
-          throw new NoSuchElementException(
-            s"$name is not a member of Enum (${Post.Content.Type.values.mkString(", ")})"
-          )
-        ),
+        .getOrElse(throw new NoSuchElementException(show"$name is not a member of Enum (${Post.Content.Type.values})")),
     _.entryName.toLowerCase(branchtalkLocale)
   )
 
