77c5611090d4a6a16cb01e745ac27653259db374
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2026-07-03 18:47:29 +0200

    Format derivation.scala (scalafmt align)

--- modules/common/src/main/scala/io/branchtalk/shared/model/derivation.scala
+++ modules/common/src/main/scala/io/branchtalk/shared/model/derivation.scala
@@ -9,13 +9,13 @@ import hearth.kindlings.catsderivation.extensions.*
 /** Recursive, fast `cats.Eq` derivation. */
 type FastEq[A] = Eq[A]
 object FastEq {
-  inline def derived[A]: Eq[A] = Eq.derived[A]
+  inline def derived[A]:                Eq[A] = Eq.derived[A]
   inline def apply[A](using ev: Eq[A]): Eq[A] = ev
 }
 
 /** Pretty, multi-line `cats.Show` derivation (a `cats.Show` with extra `showLines`). */
 type ShowPretty[A] = hearth.kindlings.catsderivation.ShowPretty[A]
 object ShowPretty {
-  inline def derived[A]: ShowPretty[A] = hearth.kindlings.catsderivation.ShowPretty.derived[A]
+  inline def derived[A]:                        ShowPretty[A] = hearth.kindlings.catsderivation.ShowPretty.derived[A]
   inline def apply[A](using ev: ShowPretty[A]): ShowPretty[A] = ev
 }
